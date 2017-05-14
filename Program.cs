280b7e5c2f6f0dc818759d418d22ecad50d9a2e8
avandehoef <avandehoef@outlook.com>
2017-05-14 11:29:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,7 +19,21 @@ namespace ConsoleApp7
 
             Console.WriteLine("Geef een getal op voor een check of dit een priemgetal is;");
 
-            Int64 getal1 = Int64.Parse(Console.ReadLine());
+            
+            string invoer = Console.ReadLine();
+            Int64 getal1;
+            bool succesvolleInvoer = Int64.TryParse(invoer, out getal1);
+
+            while (true)
+            {
+                if (invoer.Length != 0 && succesvolleInvoer == true && getal1 > 0)
+                    break;
+                Console.WriteLine("Dit is geen geldige invoer; geef een getal op groter dan 0");
+                Console.WriteLine("Geef een getal op voor een check of dit een priemgetal is");
+                invoer = Console.ReadLine();
+                succesvolleInvoer = Int64.TryParse(invoer, out getal1);
+            }
+                       
             Int64 y = getal1 -2;
             Int64 getal2;
 
